12a63590faa764fc5e3bfd13ae7d0c45c48fc98f
leoaugustov <vieira.augustoleonardo@gmail.com>
2022-12-09 00:08:59 +0000

    "Brew formula update for dlq-utils version v0.4.2"

--- Formula/dlq-utils.rb
+++ Formula/dlq-utils.rb
@@ -5,21 +5,21 @@
 class DlqUtils < Formula
   desc "Set of scripts that help with dlq message processing"
   homepage "https://github.com/leoaugustov/dlq-utils"
-  url "https://github.com/leoaugustov/dlq-utils/archive/v0.4.1.tar.gz"
-  sha256 "16ca664848558b08cc2ba59f712f326835d3887f6632b8bc6d1e634e98bda4ec"
+  url "https://github.com/leoaugustov/dlq-utils/archive/v0.4.2.tar.gz"
+  sha256 "008010f410fa42b18db53b1fbc05238c4dd00d602ff51e86813632dc32e6d451"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/leoaugustov/dlq-utils/releases/download/v0.4.1/dlq-utils-0.4.1-darwin-amd64.tar.gz"
-      sha256 "ec20a8f52fc7d21059f3a2bfaaa9b38627455b821fa30f8b03865820d8141cf7"
+      url "https://github.com/leoaugustov/dlq-utils/releases/download/v0.4.2/dlq-utils-0.4.2-darwin-amd64.tar.gz"
+      sha256 "63d4656145505aa7e6fd529e21985fe5f4e8c43755da0649eaacf4b09d15047b"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/leoaugustov/dlq-utils/releases/download/v0.4.1/dlq-utils-0.4.1-linux-amd64.tar.gz"
-      sha256 "fa9661dd64a38a687a892bef698e7e56ffa807f643492752f73dc436fe9158cd"
+      url "https://github.com/leoaugustov/dlq-utils/releases/download/v0.4.2/dlq-utils-0.4.2-linux-amd64.tar.gz"
+      sha256 "1fbea207a6e2d46b742243cfa73cfae7314a2186f35507ab7f0e240712b1b7f7"
     end
   end
 
